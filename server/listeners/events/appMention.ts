4a7374c77336121d1eade4b131f798fe42ebffc2
Devarsh <75148774+webdev03@users.noreply.github.com>
2026-03-11 18:50:46 +1300

    feat: add pinging functionality

--- server/listeners/events/appMention.ts
+++ server/listeners/events/appMention.ts
@@ -1,28 +1,94 @@
 import type { AllMiddlewareArgs, SlackEventMiddlewareArgs } from '@slack/bolt';
+import { eq } from 'drizzle-orm';
+import { db } from '../../db';
+import { groups } from '../../db/schema';
 
 export const appMention = async ({
 	event,
 	client,
 	context,
 }: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_mention'>) => {
+	if (!event.user) return;
+
 	const command = event.text
 		.split(`<@${context.botUserId}>`)[1]
 		?.trim()
 		.split(' ')[0]
-		?.trim();
+		?.trim()
+		.toLowerCase();
 	if (!command) {
-		if (event.user)
-			await client.chat.postEphemeral({
-				text: "Couldn't parse message",
-				channel: event.channel,
-				user: event.user,
-			});
+		await client.chat.postEphemeral({
+			text: "Couldn't parse message - try visting my App Home for more details",
+			channel: event.channel,
+			user: event.user,
+		});
 		return;
 	}
-	await client.chat.postEphemeral({
-		text: command,
-		channel: event.channel,
-		// biome-ignore lint/style/noNonNullAssertion: quick testing
-		user: event.user!,
+
+	const group = await db.query.groups.findFirst({
+		where: eq(groups.name, command),
 	});
+
+	if (!group) {
+		await client.chat.postEphemeral({
+			text: `Couldn't find a group named '${command}' - if you want to make a new group, visit my App Home`,
+			channel: event.channel,
+			user: event.user,
+		});
+		return;
+	}
+
+	if (!group.admins.includes(event.user)) {
+		await client.chat.postEphemeral({
+			text: `Sorry, you can't ping the group '${command}' because you are not an admin of it!`,
+			channel: event.channel,
+			user: event.user,
+		});
+		return;
+	}
+
+	await client.chat.postEphemeral({
+		text: `Sending pings to ${group.members.length} ${group.members.length === 1 ? 'member' : 'members'} of '${command}'`,
+		channel: event.channel,
+		user: event.user,
+	});
+
+	const permalink = (
+		await client.chat.getPermalink({
+			channel: event.channel,
+			message_ts: event.ts,
+		})
+	).permalink;
+
+	if (!permalink) {
+		console.error('Failed to fetch permalink');
+		return;
+	}
+
+	const errorMembers: string[] = [];
+
+	for (const member of group.members) {
+		try {
+			await client.chat.postMessage({
+				markdown_text: `<@${event.user}> has [pinged the group '${command}'](${permalink})!`,
+				channel: member,
+			});
+		} catch (error) {
+			console.error(error);
+			errorMembers.push(member);
+		}
+	}
+	if (errorMembers.length > 0) {
+		await client.chat.postEphemeral({
+			text: `Failed to ping ${errorMembers.length} ${errorMembers.length === 1 ? 'member' : 'members'} of '${command}' - ${errorMembers.map((x) => `<@${x}>`).join(' ')}`,
+			channel: event.channel,
+			user: event.user,
+		});
+	} else {
+		await client.chat.postEphemeral({
+			text: `Successfully pinged ${group.members.length} ${group.members.length === 1 ? 'member' : 'members'} of '${command}'!`,
+			channel: event.channel,
+			user: event.user,
+		});
+	}
 };
---
+++ server/reserved.ts
@@ -0,0 +1,17 @@
+// Reserved group names
+export const RESERVED = [
+	'channel',
+	'here',
+	'everyone',
+	'group',
+	'create',
+	'migrate',
+	'add',
+	'remove',
+	'join',
+	'leave',
+	'null',
+	'undefined',
+];
+
+export const ALLOWED_CHARS = 'qwertyuiopasdfghjklzxcvbnm1234567890_-'.split('');
