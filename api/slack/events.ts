0f702138676cad580eb92ec630bfe3f180204097
Devarsh <75148774+webdev03@users.noreply.github.com>
2026-03-13 23:52:12 +1300

    fix: use process.env on Vercel

--- api/slack/events.ts
+++ api/slack/events.ts
@@ -2,16 +2,15 @@
 
 import { App } from '@slack/bolt';
 import { createHandler, VercelReceiver } from '@vercel/slack-bolt';
-import { env } from '../../server/env';
 import { registerListeners } from '../../server/listeners';
 
 const receiver = new VercelReceiver({
-	signingSecret: env.SLACK_SIGNING_SECRET,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 const app = new App({
-	token: env.SLACK_BOT_TOKEN,
-	signingSecret: env.SLACK_SIGNING_SECRET,
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
 	receiver,
 	deferInitialization: true,
 });
